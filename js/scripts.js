197077afbdb67d7889da176c7f4f18dce4b5ac8f
Your Name <Cacek@users.noreply.github.com>
2017-05-04 11:58:32 +0200

    Zadanie_10.2

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+$(function(){
+    $("span:even").css('color', 'red'); //alternatywa do niżej 
+
+    /* var span = $("span");
+      span.each(function(index, element) {
+         if(index % 2 == 0) {
+             $(element).css('color', 'red');
+         }; 
+      });*/
+
+    $("p").each(function(index, element) {
+        var button = '<button class="btn" data-tmp="' + index + '">Nadus</button>';
+        $(element).append(button);
+    });
+
+    $("button").click(function(){
+        alert($(this).attr("data-tmp"));
+    });
+});
