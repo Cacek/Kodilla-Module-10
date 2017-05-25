6350dea8cd17930c19e09d6d7beebd397a2202a8
Cacek <you@example.com>
2017-05-25 16:44:25 +0200

    poprawki

--- js/scripts.js
+++ js/scripts.js
@@ -1,110 +1,113 @@
-// SLIDER
-var slider = function() {
+var slider = function () {
 
-    // Set time delay of slider
     var delay = 5000;
-
-    // Set variables
     var numImages = $('.img').length;
     var previousImage = numImages;
     var currentImage = 1;
     var nextImage = 2;
     var previewHover = false;
 
-    // Find width of container div
-    var sliderWidth = $("#slider").width() + 2;
-
-    // Give images same width as slider
-    var sizeImages = function() {
+    
+    var sliderWidth = $("#slider").width();
+    var sizeImages = function () {
         $("img").width(sliderWidth);
     };
     sizeImages();
 
-    // Create as many navigation dots as there are pictures
     for (var i = 1; i <= numImages; i++) {
         $('#dots').find('ul').append($('<li class="dot ' + i + '""></li>'));
     }
 
-    // Set initial position of images
-    var resetImages = function() {
-        $('.img').css({"left": sliderWidth+"px"});
-        $('.img').first().css({"left": "0px"});
+    var resetImages = function () {
+        $('.img').css({
+            "left": sliderWidth + "px"
+        });
+        $('.img').first().css({
+            "left": "0px"
+        });
         $('.1').addClass("active");
     };
     resetImages();
 
-    // Slide to next image
-    var slideNextImageLeft = function() {
-        $('.image-' + nextImage).css({"left": sliderWidth+"px"});
-        $('.image-' + currentImage).animate({left: sliderWidth * -1}, 1000);
-        $('.image-' + nextImage).animate({left: "0px"}, 1000);
+    var slideNextImageLeft = function () {
+        $('.image-' + nextImage).css({
+            "left": sliderWidth + "px"
+        });
+        $('.image-' + currentImage).animate({
+            left: sliderWidth * -1
+        }, 1000);
+        $('.image-' + nextImage).animate({
+            left: "0px"
+        }, 1000);
         currentImage = nextImage;
         increaseImages();
     };
 
-    // Slide to the previous image
-    var slidePreviousImageRight = function() {
-        $('.image-' + previousImage).css({"left": (sliderWidth * -1)+"px"});
-        $('.image-' + currentImage).animate({left: sliderWidth}, 1000);
-        $('.image-' + previousImage).animate({left: "0px"}, 1000);
+    var slidePreviousImageRight = function () {
+        $('.image-' + previousImage).css({
+            "left": (sliderWidth * -1) + "px"
+        });
+        $('.image-' + currentImage).animate({
+            left: sliderWidth
+        }, 1000);
+        $('.image-' + previousImage).animate({
+            left: "0px"
+        }, 1000);
         currentImage = previousImage;
         increaseImages();
     };
 
-    // Shift which images are next and previous
-    var increaseImages = function() {
-        if(currentImage === numImages) {
+    var increaseImages = function () {
+        if (currentImage === numImages) {
             nextImage = 1;
             previousImage = currentImage - 1;
         } else {
             nextImage = currentImage + 1;
-            if(currentImage === 1) {
+            if (currentImage === 1) {
                 previousImage = numImages;
             } else {
                 previousImage = currentImage - 1;
             }
         }
-        // Change dots status
+    
         $('#dots').find('li').removeClass("active");
         $('#dots').find('.' + currentImage).addClass("active");
-        // If hovering over preview buttons, change picture in preview
-        if(previewHover !== false) {
+        
+        if (previewHover !== false) {
             showPreview();
         }
     };
 
-    // Set initial slider interval
-    moveImages = setInterval(function() {
+    
+    moveImages = setInterval(function () {
         slideNextImageLeft();
     }, delay);
 
-    // When next button is clicked
-    $('.next').click(function() {
+    
+    $('.next').click(function () {
         clearInterval(moveImages);
-        moveImages = setInterval(function() {
+        moveImages = setInterval(function () {
             slideNextImageLeft();
         }, delay);
         slideNextImageLeft();
     });
 
-    // When previous button is clicked
-    $('.previous').click(function() {
+    $('.previous').click(function () {
         clearInterval(moveImages);
-        moveImages = setInterval(function() {
+        moveImages = setInterval(function () {
             slideNextImageLeft();
         }, delay);
         slidePreviousImageRight();
     });
 
-    // When a navigation dot is clicked
-    $('.dot').click(function() {
+    $('.dot').click(function () {
         buttonPressed = $('li').index(this) + 1;
-        if(buttonPressed !== currentImage) {
+        if (buttonPressed !== currentImage) {
             clearInterval(moveImages);
-            moveImages = setInterval(function() {
+            moveImages = setInterval(function () {
                 slideNextImageLeft();
             }, delay);
-            if(currentImage < buttonPressed) {
+            if (currentImage < buttonPressed) {
                 nextImage = buttonPressed;
                 slideNextImageLeft();
             } else {
@@ -114,30 +117,34 @@ var slider = function() {
         }
     });
 
-    // Show a preview of next or previous image on hover
-    $('.nav').on('mouseenter', function() {
+    $('.nav').on('mouseenter', function () {
         previewHover = $('.nav').index(this) + 1;
         showPreview();
-    }).on('mouseleave', function() {
+    }).on('mouseleave', function () {
         previewHover = false;
-        $(".preview").css({"background-image": "none"});
+        $(".preview").css({
+            "background-image": "none"
+        });
     });
 
-    var showPreview = function() {
+    var showPreview = function () {
         var whichSide = previewHover;
         var miniWidth = 100;
         var whichImage;
-        if(whichSide === 1) {
+        if (whichSide === 1) {
             whichImage = previousImage;
         } else {
             whichImage = nextImage;
         }
         var previewImage = $('.image-' + whichImage).find('img').attr("src");
-        $(".preview:nth-child(" + whichSide + ")").css({"background-image": "url(" + previewImage + ")", "background-size": "cover"});
+        $(".preview:nth-child(" + whichSide + ")").css({
+            "background-image": "url(" + previewImage + ")",
+            "background-size": "cover",
+        });
     };
 };
 
-$(document).ready(function() {
+$(document).ready(function () {
 
     slider();
 
